b13156c67d4775078037038f93f2c19e232a0b61
Mitanshu Khandal <mitukhandal01@gmail.com>
2026-09-15 12:19:11 +0530

    Step 5: mlp_forward

--- model.py
+++ model.py
@@ -43,8 +43,18 @@ def linear_forward(x, w, b):
 def relu_forward(x):
     return np.maximum(x,0.0)
 
-# Step 5 - mlp_forward (not yet solved)
-# TODO: implement
+# Step 5 - mlp_forward
+def mlp_forward(x, params):
+    z1 = linear_forward(x,params["W1"], params["b1"])
+    a1 = relu_forward(z1)
+    z2 = linear_forward(a1,params["W2"],params["b2"])
+    cache = {
+        "x" : x,
+        "z1"  : z1,
+        "a1"  :a1,
+        "z2" : z2
+    }
+    return z2, cache
 
 # Step 6 - mse_loss_and_grad (not yet solved)
 # TODO: implement
