dcff65886fc05aedbad8aed48592d5dddd4ef359
Mitanshu Khandal <mitukhandal01@gmail.com>
2026-09-15 16:39:25 +0530

    Step 6: mse_loss_and_grad

--- model.py
+++ model.py
@@ -56,8 +56,12 @@ def mlp_forward(x, params):
     }
     return z2, cache
 
-# Step 6 - mse_loss_and_grad (not yet solved)
-# TODO: implement
+# Step 6 - mse_loss_and_grad
+def mse_loss_and_grad(y_pred, y_true):
+    diff = y_pred  - y_true
+    loss = np.mean(diff**2)
+    dy_pred = (2/y_pred.size) * diff
+    return float(loss) , dy_pred
 
 # Step 7 - linear_backward (not yet solved)
 # TODO: implement
